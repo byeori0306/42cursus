fe9a39759e1569548c3c3965a47d327cb23adbbf
Dahee Park <byeori0306@gmail.com>
2020-10-18 14:24:08 +0900

    libft

---
+++ libft/ft_itoa.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dahpark <dahpark@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/18 10:06:10 by dahpark           #+#    #+#             */
+/*   Updated: 2020/10/18 14:22:49 by dahpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		get_len(int n)
+{
+	unsigned int num;
+	unsigned int len;
+
+	if (n <= 0)
+	{
+		len = 1;
+		num = n * -1;
+	}
+	else
+	{
+		len = 0;
+		num = n;
+	}
+	while (num > 0)
+	{
+		num /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	unsigned int	i;
+	unsigned int	len;
+	unsigned int	num;
+	char			*arr;
+
+	i = 0;
+	len = get_len(n);
+	if ((arr = (char *)malloc(sizeof(char) * (len + 1))) == NULL)
+		return (0);
+	if (n == 0)
+		arr[0] = '0';
+	else if (n < 0)
+	{
+		arr[0] = '-';
+		num = n * -1;
+	}
+	else
+		num = n;
+	while (num > 0)
+	{
+		arr[len - (i++) - 1] = (num % 10 + '0');
+		num /= 10;
+	}
+	arr[len] = '\0';
+	return (arr);
+}
--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dahpark <dahpark@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/17 10:13:55 by dahpark           #+#    #+#             */
-/*   Updated: 2020/10/17 22:45:15 by dahpark          ###   ########.fr       */
+/*   Updated: 2020/10/18 14:23:17 by dahpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,11 @@ int		get_arr_len(char const *s, char c)
 		if (*s != c)
 		{
 			len++;
-			while (*s != c)
+			while (*s && *s != c)
 				s++;
 		}
-		s++;
+		if (*s == c)
+			s++;
 	}
 	return (len);
 }
@@ -39,25 +40,23 @@ int		get_str_len(char const *s, char c)
 	while (*s != c)
 	{
 		s++;
-		len++;	
+		len++;
 	}
 	return (len);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int i;
-	int j;
-	int arr_len;
-	int str_len;
-   	char **arr;
+	int		i;
+	int		j;
+	int		arr_len;
+	int		str_len;
+	char	**arr;
 
 	arr_len = get_arr_len(s, c);
 	if ((arr = (char **)malloc(sizeof(char *) * (arr_len + 1))) == NULL)
 		return (0);
-	
 	i = 0;
-	j = 0;
 	while (*s)
 	{
 		if (*s != c)
@@ -66,21 +65,21 @@ char	**ft_split(char const *s, char c)
 			if ((arr[i] = (char *)malloc(sizeof(char) * (str_len + 1))) == NULL)
 				return (0);
 			j = 0;
-			while (*s != c)
+			while (*s && *s != c)
 				arr[i][j++] = *s++;
 			arr[i][j] = '\0';
 			i++;
 		}
-		s++;
+		if (*s == c)
+			s++;
 	}
 	arr[arr_len] = 0;
-	
 	return (arr);
 }
-
+/*
 int main(void)
 {
-	char *s = "a0aa0aaa";
+	char *s = "0a0aa0aaa0aaaa";
 	char c = '0';
 	int i = 0;
 	char **result = ft_split(s, c);
@@ -91,3 +90,4 @@ int main(void)
 		i++;
 	}
 }
+*/
