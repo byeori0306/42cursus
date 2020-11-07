621be79786a1c99c7965e645de9c2e2de85ff73e
Dahee Park <byeori0306@gmail.com>
2020-11-07 23:45:42 +0900

    libft

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dahpark <dahpark@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/07 15:39:12 by dahpark           #+#    #+#             */
-/*   Updated: 2020/10/13 19:12:06 by dahpark          ###   ########.fr       */
+/*   Updated: 2020/11/07 23:02:01 by dahpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] != '\0' && i < n)
+	while (i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
