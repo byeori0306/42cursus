c20301fd9f3f4e76efbc56fe42cc031a337724eb
Dahee Park <dahpark@c2r9s6.42seoul.kr>
2021-02-28 03:08:05 +0900

    modify data type for read size and check norminette

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,17 +1,27 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dahpark <dahpark@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/28 02:57:06 by dahpark           #+#    #+#             */
+/*   Updated: 2021/02/28 03:03:53 by dahpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <string.h>
 #include "get_next_line.h"
 
 char	*gnl_strjoin(char *s1, char *s2)
 {
-	size_t l1;
-	size_t l2;
-	size_t len;
-	char *result;
-	
+	size_t	l1;
+	size_t	l2;
+	size_t	len;
+	char	*result;
+
 	if (s1)
 		l1 = ft_strlen(s1);
 	else
@@ -26,14 +36,13 @@ char	*gnl_strjoin(char *s1, char *s2)
 		free(s1);
 	}
 	ft_strlcpy(result + l1, s2, l2);
-	
 	return (result);
 }
 
-int	gnl_split(char **storage, char *eol, char **line)
-{	
-	size_t len;
-	
+int		gnl_split(char **storage, char *eol, char **line)
+{
+	size_t	len;
+
 	len = eol - *storage;
 	if (!(*line = (char *)malloc(sizeof(char) * (len + 1))))
 		return (-1);
@@ -41,14 +50,13 @@ int	gnl_split(char **storage, char *eol, char **line)
 		(*line)[0] = '\0';
 	else
 		ft_strlcpy(*line, *storage, len);
-	ft_memmove(*storage, eol+1, ft_strlen(*storage)-len);
-
+	ft_memmove(*storage, eol + 1, ft_strlen(*storage) - len);
 	return (1);
 }
 
-int gnl_result(size_t size, char **storage, char **line)
+int		gnl_result(ssize_t size, char **storage, char **line)
 {
-	char *eol;
+	char	*eol;
 
 	if (size < 0)
 		return (-1);
@@ -69,16 +77,16 @@ int gnl_result(size_t size, char **storage, char **line)
 	return (0);
 }
 
-int	get_next_line(int fd, char **line)
+int		get_next_line(int fd, char **line)
 {
-	size_t size;
-	char *buf;
-	static char *storage;
-	char *eol;
-	
+	ssize_t		size;
+	char		*buf;
+	static char	*storage;
+	char		*eol;
+
 	if (fd < 0 || !line || BUFFER_SIZE < 1)
 		return (-1);
-	if (!(buf = malloc(sizeof(char) * (BUFFER_SIZE + 1))))	
+	if (!(buf = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
 		return (-1);
 	while ((size = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
