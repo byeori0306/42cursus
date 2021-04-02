7a3e0b034f54fa91be35be887f49c4a10d0e6a43
byeori0306 <byeori0306@gmail.com>
2021-04-02 14:44:57 +0900

    libft

---
+++ ft_printf/libft/ft_memccpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dahpark <dahpark@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/10 13:34:16 by dahpark           #+#    #+#             */
+/*   Updated: 2020/11/14 19:54:31 by dahpark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	unsigned char		*temp;
+	const unsigned char	*s;
+	size_t				i;
+
+	temp = dst;
+	s = src;
+	i = 0;
+	while (i < n)
+	{
+		temp[i] = s[i];
+		if (s[i] == (unsigned char)c)
+			break ;
+		i++;
+	}
+	if (i == n)
+		return (0);
+	else
+		return (&dst[++i]);
+}
