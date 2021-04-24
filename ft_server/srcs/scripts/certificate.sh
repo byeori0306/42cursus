dcc2c26ce2303b44a1a702246bdb1e77a8a5cb43
Dahee Park <byeori0306@gmail.com>
2021-04-25 05:18:54 +0900

    copy key and crt, autoindex now is able to be disabled

--- ft_server/srcs/scripts/certificate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-openssl req -x509 -newkey rsa:4096 -nodes -sha256 -keyout ft_server.key -out ft_server.crt -days 365 -subj "/C=KR/ST=SEOUL/L=SEOUL/O=42seoul/OU=gun/CN=localhost"
-mv ft_server.key /etc/ssl/private
-mv ft_server.crt /etc/ssl/certs
-chmod 600 /etc/ssl/*
--- ft_server/srcs/scripts/init_container.sh
+++ ft_server/srcs/scripts/init_container.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+if [ ${AUTO} -eq 0 ];then
+	sed -i 's/autoindex on;/autoindex off;/g' /etc/nginx/sites-available/default
+fi
 service nginx start
 service php7.3-fpm start
 service php7.3-fpm status
-service mysql restart
+service mysql start
 sleep infinity
