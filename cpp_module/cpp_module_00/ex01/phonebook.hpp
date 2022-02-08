601c7b0f5f97333f87c828d47bade3a1202506ad
Dahee Park <byeori0306@gmail.com>
2022-02-08 18:54:26 +0900

    FIX : move display_contacts function

--- cpp_module/cpp_module_00/ex01/phonebook.hpp
+++ cpp_module/cpp_module_00/ex01/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: dahpark <dahpark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 15:11:58 by dahpark           #+#    #+#             */
-/*   Updated: 2021/12/26 14:36:00 by dahpark          ###   ########seoul.kr  */
+/*   Updated: 2022/02/08 18:51:39 by dahpark          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ class PhoneBook {
 	private:
 		int		cnt;
 		contact contacts[8];
+		void	display_contacts(int limit);
 	public:
 		PhoneBook();
 		void	add_contact();
-		void	display_contacts(int limit);
 		void	search_contact();
 };
 
