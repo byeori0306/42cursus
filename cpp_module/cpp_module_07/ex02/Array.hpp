b0bb27f721a0061d1700c1cb3402821f8b453f84
Dahee Park <byeori0306@gmail.com>
2022-02-19 20:48:17 +0900

    FIX : fix operator=

--- cpp_module/cpp_module_07/ex02/Array.hpp
+++ cpp_module/cpp_module_07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: dahpark <dahpark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 14:41:09 by dahpark           #+#    #+#             */
-/*   Updated: 2022/02/19 20:34:56 by dahpark          ###   ########seoul.kr  */
+/*   Updated: 2022/02/19 20:47:28 by dahpark          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ class Array {
 		unsigned int n;
 		T *arr;
 	public:
-		Array() : n(0), arr(new T[0]()) { }
-		Array(unsigned int n) : n(n), arr(new T[n]()) { }
-		Array(const Array<T>& array) : n(array.n), arr(new T[n]()) {
+		Array() : n(0), arr(new T[n]) { }
+		Array(unsigned int n) : n(n), arr(new T[n]) { }
+		Array(const Array<T>& array) : n(array.n), arr(new T[n]) {
 			for (unsigned int i = 0; i < n; i++)
 				arr[i] = array.arr[i];
 		}
@@ -31,6 +31,8 @@ class Array {
 			if (this == &array)
 				return (*this);
 			n = array.n;
+			delete[] arr;
+			arr = new T[n];
 			for (unsigned int i = 0; i < n; i++)
 				arr[i] = array.arr[i];
 			return (*this);
@@ -46,7 +48,7 @@ class Array {
 				}	
 		};
 
-		T &operator[](unsigned int idx) {
+		T& operator[](unsigned int idx) {
 			if (idx >= n)
 				throw Array<T>::outOfTheLimits();
 			return (arr[idx]);
