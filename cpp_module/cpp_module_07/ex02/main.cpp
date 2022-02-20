543591435aa259bc456454c08ab9d3387d04612f
Dahee Park <byeori0306@gmail.com>
2022-02-20 16:25:09 +0900

    FEAT : add more test case

--- cpp_module/cpp_module_07/ex02/main.cpp
+++ cpp_module/cpp_module_07/ex02/main.cpp
@@ -6,13 +6,13 @@
 /*   By: dahpark <dahpark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 14:41:12 by dahpark           #+#    #+#             */
-/*   Updated: 2022/02/06 19:55:33 by dahpark          ###   ########seoul.kr  */
+/*   Updated: 2022/02/20 16:24:33 by dahpark          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
-int main(void) {
+void	test_int(void) {
 	Array<int> array(5);
 
 	for (unsigned int i = 0; i < 5; i++) {
@@ -48,4 +48,43 @@ int main(void) {
 	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
+}
+
+void	test_char(void) {
+	Array<char> array(5);
+	std::string str = "abcde";
+
+	for (unsigned int i = 0; i < 5; i++) {
+		array[i] = str[i];
+	}
+
+	for (unsigned int i = 0; i < 5; i++) {
+		std::cout << "array : " << array[i] << std::endl;
+	}
+
+	Array<char> test(array);
+	
+	for (unsigned int i = 0; i < 5; i++) {
+		std::cout << "test : "  << test[i] << std::endl;
+	}
+
+	try {
+		array[2] = 'x';
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	for (unsigned int i = 0; i < 5; i++) {
+		std::cout << "array : " << array[i] << std::endl;
+	}
+
+	for (unsigned int i = 0; i < 5; i++) {
+		std::cout << "test : "  << test[i] << std::endl;
+	}
+}
+
+int main(void) {
+	test_int();
+	std::cout << std::endl;
+	test_char();
 }
