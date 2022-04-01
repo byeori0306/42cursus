401323c9acf92db5094ed7e286fb52588bd0e90a
Dahee Park <byeori0306@gmail.com>
2022-04-01 21:58:29 +0900

    ADD : create database

--- inception/srcs/requirements/mariadb/tools/setup.sh
+++ inception/srcs/requirements/mariadb/tools/setup.sh
@@ -1,1 +1,8 @@
-mysqld_safe
+service mysql start
+
+mysql -u root -e "CREATE DATABASE ${MARIADB_DB};"
+mysql -u root -e "CREATE USER '${MARIADB_ID}'@'localhost' IDENTIFIED BY '${MARIADB_PW}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_ID}'@'localhost' WITH GRANT OPTION;"
+mysql -u root -e "UPDATE mysql.user SET Password = PASSWORD('{MARIADB_ROOT_PW}') WHERE User = 'root';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
