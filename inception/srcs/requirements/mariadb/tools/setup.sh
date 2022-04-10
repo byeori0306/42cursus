4c97feebcf4fa0dd535ad30676a3bb81e4cb00da
Dahee Park <byeori0306@gmail.com>
2022-04-10 20:19:40 +0900

    connect mariadb with php-fpm

--- inception/srcs/requirements/mariadb/tools/setup.sh
+++ inception/srcs/requirements/mariadb/tools/setup.sh
@@ -1,8 +1,8 @@
 service mysql start
 
 mysql -u root -e "CREATE DATABASE ${MARIADB_DB};"
-mysql -u root -e "CREATE USER '${MARIADB_ID}'@'localhost' IDENTIFIED BY '${MARIADB_PW}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_ID}'@'localhost' WITH GRANT OPTION;"
+mysql -u root -e "CREATE USER '${MARIADB_ID}'@'%' IDENTIFIED BY '${MARIADB_PW}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO '${MARIADB_ID}'@'%';"
 mysql -u root -e "UPDATE mysql.user SET Password = PASSWORD('{MARIADB_ROOT_PW}') WHERE User = 'root';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
