c117555d2cfdeae6a61d6e690152e833ec128831
Dahee Park <byeori0306@gmail.com>
2022-04-12 01:45:05 +0900

    FEAT : use environment variables in mariadb container

--- inception/srcs/requirements/mariadb/tools/setup.sh
+++ inception/srcs/requirements/mariadb/tools/setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 service mysql start
 
 mysql -u root -e "CREATE DATABASE ${MARIADB_DB};"
@@ -6,3 +7,8 @@ mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO '${MARIADB_ID}'@'%';"
 mysql -u root -e "UPDATE mysql.user SET Password = PASSWORD('{MARIADB_ROOT_PW}') WHERE User = 'root';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
+sleep 3;
+
+service mysql stop
+
+exec "$@"
