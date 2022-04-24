e78e1de78299fd51c5cb9ad3a5d2a0f26e02ef11
Dahee Park <byeori0306@gmail.com>
2022-04-24 22:29:23 +0900

    ADD : ft_containers 시작

---
+++ ft_containers/Vector.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dahpark <dahpark@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/24 22:17:32 by dahpark           #+#    #+#             */
+/*   Updated: 2022/04/24 22:26:26 by dahpark          ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP_
+#define VECTOR_HPP_
+
+#include <memory>
+
+template < class T, class Alloc = std::allocator<T> >
+class vector {
+	
+};
+
+#endif
