043b475820ac79496635649e2448d3b25eaf2159
Dahee Park <byeori0306@gmail.com>
2022-04-25 16:34:41 +0900

    ADD : Add vector member type

--- ft_containers/Vector.hpp
+++ ft_containers/Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: dahpark <dahpark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 22:17:32 by dahpark           #+#    #+#             */
-/*   Updated: 2022/04/24 22:26:26 by dahpark          ###   ########seoul.kr  */
+/*   Updated: 2022/04/25 16:32:22 by dahpark          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,26 @@
 
 #include <memory>
 
-template < class T, class Alloc = std::allocator<T> >
-class vector {
-	
-};
+namespace ft {
+	template < class T, class Alloc = std::allocator<T> >
+	class vector {
+		private:
+		public:
+			typedef T											value_type;
+			typedef Alloc										allocator_type;
+			typedef typename allocator_type::reference			reference;
+			typedef typename allocator_type::const_reference 	const_reference;
+			typedef typename allocator_type::pointer			pointer;
+			typedef typename allocator_type::const_pointer		const_pointer;
+			/*
+			typedef _	iterator;
+			typedef _	const_iterator;
+			typedef _	reverse_iterator;
+			typedef _	const_reverse_iterator;
+			typedef _	difference_type;
+			*/
+			typedef size_t										size_type;
+	};
+}
 
 #endif
